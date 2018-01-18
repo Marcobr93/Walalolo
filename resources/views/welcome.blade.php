16cda75c2147be562474a6c105ad9dd92f3d9080
Marcobr93 <marcoabr93@gmail.com>
2018-01-18 20:46:46 +0100

    Subida del proyecto.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Walalolo
+                    Walalolol
                 </div>
 
                 <div class="links">
