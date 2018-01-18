fd879433911df85842b7d46a4357f7f69183bc79
Marcobr93 <marcoabr93@gmail.com>
2018-01-18 20:52:08 +0100

    Creación del proyecto

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Walalolol
+                    Walalolo
                 </div>
 
                 <div class="links">
